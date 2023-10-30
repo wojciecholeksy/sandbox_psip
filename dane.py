2746092755e50c7e0c601738be66da50b647f047
wojciecholeksy <149381966+wojciecholeksy@users.noreply.github.com>
2023-10-30 14:45:52 +0100

    add read funcionalities

---
+++ dane.py
@@ -0,0 +1,11 @@
+users_list:list = [
+            {"name":"Wojciech", "nick":"PanWowo", "posts":1},   #lista osób
+            {"name": "Andrzej", "nick":"Endrju",  "posts":60},
+            {"name":"Bartosz", "nick":"Baran", "posts":31231},
+            {"name":"Kamil", "nick":"Kola", "posts":123},
+            {"name":"Marek", "nick":"Wiess", "posts":0},
+            {"name":"Wiktoria", "nick":"Witka", "posts":360},
+            {"name":"Wiktoria", "nick":"Wiki", "posts":123},
+            {"name":"Monika", "nick":"Monia", "posts":1_000_000},
+            {"name":"Kasia", "nick":"Kkk", "posts":1500}
+]
