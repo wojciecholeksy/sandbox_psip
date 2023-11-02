ffd69f326bc4b5a2f7573bb459acae0691f1f11c
wojciecholeksy <149381966+wojciecholeksy@users.noreply.github.com>
2023-11-02 21:42:22 +0100

    do_nauki

---
+++ do_nauki.py
@@ -0,0 +1,96 @@
+'''stan_konta = input('Podaj stan konta:') //input-wyświetla tekst
+stan_konta = int(stan_konta)// int-rodzaj zmiennej
+stan_konta = stan_konta + 500* 2
+print(stan_konta) //print Wyświetl '''
+import random
+
+'''x = 9
+y = x//3
+x =x**3
+print(y)'''
+'''temperature = input('Jaką mamy obecnie temperaturę?')
+szczęśliwy = False
+temperature = int(temperature)
+if temperature > 10 and szczęśliwy: /and obydwa warunki muszą być spełnione
+     print('Wyjdź, bo jest ciepło')
+elif temperature > -10 or szczęśliwy:  / or jeden warunek wystarczy
+     print('Ubierz się ciepło')
+else:
+    print('Nie wychodź')'''
+
+'''for i in range(1,10,2):
+   if i ==5:
+      continue
+   print(i)'''
+
+
+'''temperature = 11'''
+
+'''while temperature > 10:
+    print('Temperatura jest ok', temperature)
+    temperature  -= 1'''
+
+
+'''oceny = [4,5, 3, 2, 1, 6, 4]'''
+
+'''for i in range(len(oceny)) :
+    print(oceny[i], end =' ')
+
+for ocena in oceny:
+        print(ocena, end=' ')'''
+'''print()'''
+
+'''for i, ocena in enumerate(oceny) :
+    if i%2 == 0 and ocena >= 3 :
+        print(i, ocena)
+
+
+for i, ocena in enumerate(oceny):
+    oceny[i] += 1
+
+print(oceny)'''
+
+# import module
+'''import random as r
+
+ph_no = []
+
+# pierwsza cyfra powinna przyjąć wartość od 0 do 9
+ph_no.append(r.randint(0, 9))'''
+
+'''# pętla for służy do dołączenia 8 pozostałych liczb.
+# pozostałe 8 cyfr będzie z zakresu  od 0 do 9.
+for i in range(0, 9):
+    ph_no.append(r.randint(0, 9))
+
+# printing the number
+for i in ph_no:
+if
+    print(i, i, , end="")'''
+
+
+'''import random as r
+ph_nu = []
+ph_nu1 = []
+ph_nu2 = []
+ph_nu3 = []
+for i in range(0, 3):
+    ph_nu1.append(r.randint(0, 9))
+    ph_nu2.append(r.randint(0, 9))
+    ph_nu3.append(r.randint(0, 9))
+
+ph_nu1 = ph_nu1 
+ph_nu2 = ph_nu2
+ph_nu3 = ph_nu3
+
+areacode= 48
+/////#///print('+','(',areacode,')', ph_nu1 ,ph_nu2, ' ', ph_nu3)'''
+
+ph_nu = []
+ph_nu.append('+48 ')
+for i in range(9):
+    if   i == 3 or i ==6:
+        ph_nu.append('-')
+    ph_nu.append(str(random.randint(0,9)))
+print(''.join(ph_nu))
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from dane import users_list #import biblioteki lub funkcji lub zmiennej
 
-for user in users_list:   #uzytkownik w liście uzytkowników
-    print(f'Twój znajomy {user ["nick"]} dodał {user ["posts"]} postów ')
+#for user in users_list:   #uzytkownik w liście uzytkowników
+   # print(f'Twój znajomy {user ["nick"]} dodał {user ["posts"]} postów ')
 
 
 # pseudokod pętli w pythonie instrukcja: for item in nazwa_listy:
 #print(f'Twój znajomy {zmienna_na_dane[0]["nick"]} opublikował  {zmienna_na_dane[0]["posts"]} postów!!!')
-print(f" Czas odejść od komputera i wyjść z domu !!!")
+#print(f" Czas odejść od komputera i wyjść z domu !!!")
