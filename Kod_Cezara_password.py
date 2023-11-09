190f58d49343c6a62df8069668761771131750be
wojciecholeksy <149381966+wojciecholeksy@users.noreply.github.com>
2023-11-09 18:20:19 +0100

    Update Kod_Cezara_password.py

--- Kod_Cezara_password.py
+++ Kod_Cezara_password.py
@@ -3,15 +3,16 @@ def kod_cezara(tekst):
     for znak in tekst:
         if znak.isalpha():
             if znak.islower():
-                przesuniety_znak = chr((ord(znak) - ord('a') + 1) % 26 + ord('a'))
+                przesuniety_znak = chr((ord(znak) - ord('a') + 3) % 26 + ord('a'))
             else:
-                przesuniety_znak = chr((ord(znak) - ord('A') + 1) % 26 + ord('A'))
+                przesuniety_znak = chr((ord(znak) - ord('A') + 3) % 26 + ord('A'))
         else:
             przesuniety_znak = znak
         przesuniety_tekst += przesuniety_znak
     return przesuniety_tekst
-tekst_do_przesuniecia = input("Podaj wiadomość do zakodowania: ")
-przesuniety_tekst = kod_cezara(tekst_do_przesuniecia)
+tekst_do_przesuniecia = "\nABC DEF\nTERA EST ROTUNDA"
 
-print("Oryginalny tekst", tekst_do_przesuniecia)
-print("Przesunięty tekst", przesuniety_tekst)
+przesuniety_tekst = kod_cezara(tekst_do_przesuniecia.upper())
+
+print("Input: ", tekst_do_przesuniecia.upper() )
+print("Output: ", przesuniety_tekst.upper())
