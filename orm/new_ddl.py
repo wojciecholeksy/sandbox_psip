04cd98ea73a288a25d9ca7a152110a1e35d9202f
wojciecholeksy <149381966+wojciecholeksy@users.noreply.github.com>
2023-12-14 11:29:05 +0100

    wip_add orm create table and cru

--- orm/new_ddl.py
+++ orm/new_ddl.py
@@ -1,16 +1,21 @@
-import random
+#     DDL
 
+import random
 import sqlalchemy
 import os
 import sqlalchemy.orm
 from dotenv import load_dotenv
 from faker import Faker
 from geoalchemy2 import Geometry
+from .dml import User
+
 
 load_dotenv()
 
 
-##paramaetr wywołujący parametr URL z sqlalch
+
+
+def main(##paramaetr wywołujący parametr URL z sqlalch
 db_params = sqlalchemy.URL.create(
     drivername= 'postgresql+psycopg2',
     username=os.getenv("POSTGRES_USER"),
@@ -25,18 +30,6 @@ engine = sqlalchemy.create_engine(db_params)
 #definiujemy połączenie do silnika
 connection = engine.connect()
 
-Base= sqlalchemy.orm.declarative_base()
-
-class User(Base):
-    __tablename__ = "mm_table"
-
-    id = sqlalchemy.Column(sqlalchemy.Integer(), primary_key=True) #dla bazy danych intiger automatyczie taje typ serial (autonumerowanie)
-    name = sqlalchemy.Column(sqlalchemy.String(100), nullable=True)
-    location = sqlalchemy.Column('geom', Geometry(geometry_type='POINT', srid=4326), nullable=True)
-
-
-
-Base.metadata.create_all(engine)
 
 
 ### Create, tworzty sesje(połaczenie) z naszą bazą danych z której/do któej będziemy coś przestłać
@@ -76,4 +69,6 @@ session.commit()
 
 session.flush()
 connection.close()
-engine.dispose()
+engine.dispose()):
+
+
